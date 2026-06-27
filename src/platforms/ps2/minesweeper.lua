bd280bb69a31914507473eb37e1801a62e3a555f
M0JXD <jamiedrinkell@gmail.com>
2026-06-27 18:48:37 +0100

    Rename PS2 file

--- src/platforms/ps2/minesweeper.lua
+++ src/platforms/ps2/minesweeper.lua
@@ -6,18 +6,14 @@ Screen.setMode(NTSC, 640, 448, CT24, INTERLACED, FIELD)
 
 --]]
 
+local swpr = require('logic')
+
 Font.fmLoad()
 
 local function menu()
 
 	return choice
 end
-
--- Given the users first "safe" guess, place the mines anywhere but there
-local function place_mines(x, y, mode)
-	math.randomseed(os.clock())
-end
-
 
 
 -- START --
