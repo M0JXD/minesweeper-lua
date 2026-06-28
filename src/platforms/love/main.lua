cf9ce7699d6fd1959ff39d38e0f52576d4fb3fd7
M0JXD <jamiedrinkell@gmail.com>
2026-06-28 19:39:30 +0100

    Implement basic menu

--- src/platforms/love/main.lua
+++ src/platforms/love/main.lua
@@ -10,13 +10,12 @@ Expert: ~16px
 
 local swpr = require('logic')
 local cell_imgs, start_menu, arrow
-local mode, size, scale = 'b', 64
-local columns, rows, board = swpr.get_details(mode)
-
+local mode, size, scale = 0, 64
+local columns, rows, board
 
 local function draw_menu()
 	love.graphics.draw(start_menu)
-	love.graphics.draw(arrow, 110, 340 + 0)
+	love.graphics.draw(arrow, 110, 340 + mode)
 end
 
 local function draw_game()
@@ -28,7 +27,32 @@ local function draw_game()
 	end
 end
 
-local draw = draw_menu
+function menu_mousemoved(x, y, dx, dy, istouch)
+	if y < 420 then mode = 0
+	elseif y < 540 then mode = 105
+	else mode = 205 end
+end
+
+function menu_mousepressed(x, y, button, istouch, presses)
+	if y < 420 then mode = 'b'
+	elseif y < 540 then mode = 'i'
+	else mode = 'e' end
+	columns, rows, board = swpr.get_details(mode)
+	love.draw = draw_game
+	love.mousemoved = game_mousemoved
+	love.mousepressed = game_mousepressed
+end
+
+function game_mousemoved(x, y, dx, dy, istouch)
+
+end
+
+function game_mousepressed(x, y, button, istouch, presses)
+
+end
+
+love.mousemoved = menu_mousemoved
+love.mousepressed = menu_mousepressed
 
 function love.load()
 	love.window.setTitle('Lua Minesweeper - LÖVE Platform')
@@ -36,7 +60,6 @@ function love.load()
 
 	start_menu = love.graphics.newImage("assets/start_menu.png")
 	arrow = love.graphics.newImage("assets/arrow.png")
-
 	cell_imgs = love.graphics.newArrayImage{
 		-- LuaFormatter off
 		"assets/open.png",
@@ -55,13 +78,9 @@ function love.load()
 		"assets/mine.png"
 		-- LuaFormatter on
 	}
-
-
 end
 
-function love.draw()
-	draw()
-end
+love.draw = draw_menu
 
 function love.update()
 	size = mode == 'b' and 64 or mode == 'i' and 32 or 16
